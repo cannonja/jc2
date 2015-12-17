e5242a997b1503d0059bba1ca7ec219ad465116d
cannonja <jack_cannon@live.com>
2015-12-17 03:51:15 -0800

    Added test notes

--- Test/test_image_class.py
+++ Test/test_image_class.py
@@ -7,7 +7,7 @@ ic = imp.load_source('image_class', 'C:\\Users\\Jack2\\Google Drive\\Grad School
 ##Check image object created correctly
 image1 = ic.image_class("Bears.jpg")
 image2 = ic.image_class("Bears.jpg")
-#image1.im.show()
+image1.im.show()
 
 ##Check that get_flattened returns 1D array
 ##Should duplicate calling np.flatten() method
@@ -15,6 +15,12 @@ print (image1.get_1d().shape)
 print (all(image1.get_1d() == image1.data.flatten()))
 
 ##Check gray scale conversion
-image1.convert_gray()
-#image1.im.show()
+#image1.convert_gray()
+image1.im = image1.im.convert('LA')
+image1.im.show()
 #image1.save_image("BearsGray.jpg")
+
+
+#####Need to write convert_gray method from scratch:
+#####  1) Take 3D array and convert to 2D - each pixel has length 1 instead of 3
+#####  2) Figure out how to save 2D non-RGB array as an image
