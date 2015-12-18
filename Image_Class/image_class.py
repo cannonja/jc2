69377dcae6368d78d97401e730ab303f7149ce87
cannonja <jack_cannon@live.com>
2015-12-17 19:19:48 -0800

    Finished implementing class

--- Image_Class/image_class.py
+++ Image_Class/image_class.py
@@ -7,7 +7,9 @@ class image_class:
     def __init__(self, file_path):
         self.im = Image.open(file_path)
         self.data = np.asarray(self.im)
-        self.data.flags.writeable = True        
+        self.data.flags.writeable = True
+        self.data_orig = self.data
+        self.data_gray = np.zeros((self.data.shape[0], self.data.shape[1]))
 
     def get_wh(self):
         return (self.data.shape[1], self.data.shape[0])
@@ -18,16 +20,28 @@ class image_class:
     def convert_gray(self):
         'Use luminosity formula: 0.21R + 0.72G + 0.07B'
         self.data = self.data.astype(float)
-        self.data[:,:,0] *= 0.21
-        self.data[:,:,1] *= 0.72
-        self.data[:,:,2] *= 0.07
-        self.data = self.data.astype('uint8')        
+        self.data_gray = (self.data[:,:,0] * 0.21 + self.data[:,:,1] * 0.72
+                + self.data[:,:,2] * 0.07)
+        self.data_gray = self.data_gray.astype('uint8')
+        self.im = Image.fromarray(self.data_gray)
+
+    def set_block(self, coord, size, rgb):
+        'Add exception if arguments are out of range?'
+        length = coord[0] + size[0]
+        width = coord[1] + size[1]
+        self.data[coord[0]:length, coord[1]:width, 0] = rgb[0]
+        self.data[coord[0]:length, coord[1]:width, 1] = rgb[1]
+        self.data[coord[0]:length, coord[1]:width, 2] = rgb[2]
+        self.data = self.data.astype('uint8')
         self.im = Image.fromarray(self.data)
+
+    def reset_image(self):
+        self.im = Image.fromarray(self.data_orig)
 
     def save_image(self, new_path):
         #self.im = Image.fromarray(self.data)
         self.im.save(new_path)
-        
+
     
 
 
