c02e97e973138c7187cf90240026e37857278e16
cannonja <jack_cannon@live.com>
2015-12-22 08:24:56 -0800

    Cleaned up and wrote print_meta

--- MNIST_Load/mnist_load.py
+++ MNIST_Load/mnist_load.py
@@ -1,38 +1,28 @@
 import os
 import struct
 
-os.chdir("C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\MNIST")
-files = os.listdir()[2:]
+os.chdir("C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load")
+files = os.listdir()[1:]
 
-file = open(files[0], 'r+b')
-'''
-magic_num = file.read(4)
-magic_num = struct.unpack('>i', magic_num)
-num_images = file.read(4)
-num_images = struct.unpack('>i', num_images)
-num_rows = file.read(4)
-num_rows = struct.unpack('>i', num_rows)
-num_col = file.read(4)
-num_col = struct.unpack('>i', num_col)
-'''
+file_name = files[0]
 
-meta = file.read(16)
-meta = struct.unpack('>4i', meta)
 
-'''
-print ("File name:", file.name)
-print ("Magic number:", magic_num[0])
-print ("Number of images:", num_images[0])
-print ("Rows:", num_rows[0])
-print ("Columns:", num_col[0])
-'''
+def print_meta(file_name):
+    in_file = open(file_name, 'r+b')
+    
+    if (file_name.find("image") != -1):
+        meta_data = in_file.read(16)
+        meta_data = struct.unpack('>4i', meta_data)
+        meta_labels = ("Magic number", "Number of images", "Rows", "Columns")
+    else:
+        meta_data = in_file.read(8)
+        meta_data = struct.unpack('>2i', meta_data)
+        meta_labels = ("Magic number", "Number of items")        
 
-'''
-print ("\nFirst Image:")
-for i in range(0,28):
-    pixel = file.read(28)
-    print ("Row ", i + 1, ": ", pixel, sep="")
-'''
+    print ("File name:", in_file.name)
+    for (i,j) in zip(meta_labels, meta_data):
+        print (i, ": ", j, sep="")
+    print ("\n")
 
-file.close()
+    in_file.close()
 
