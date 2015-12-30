aad6d8bd2503b95ec6d456eab5015d880873f6b7
cannonja <jack_cannon@live.com>
2015-12-29 19:59:55 -0800

    Reconcile with tlab machine

--- Test/test_mnist_load.py
+++ Test/test_mnist_load.py
@@ -2,27 +2,42 @@ import imp
 import os
 
 ##Import module in module object
-
+'''
 ##Home machine import
-module = imp.load_source('mnist_load',
-                         'C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load\\mnist_load.py')
+in_module = "C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\
+                MNIST_Load\\mnist_load.py"
+module = imp.load_source('mnist_load', in_module)
+'''
+
+
 ##tlab machine import
-#module = imp.load_source('mnist_load', '/u/jc2/dev/jc2/Image_Class/image_class.py')
+in_module = "../MNIST_Load/mnist_load.py"
+module = imp.load_source('mnist_load', in_module)
+
 
 ##Built list of files to iterate through
-files = os.listdir()
+os.chdir("../MNIST_Load")
+files = os.listdir("../MNIST_Load")
 file_list = []
-for i in files:    
+for i in files:
     if (i.find("idx") != -1):
         file_list.append(i)
-    
+'''
 ##Check print_meta function
 for name in (file_list):
     module.print_meta(name)
+'''
 
 ##Check save_images
-output_path = "C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
-module.save_images(file_list[0], output_path, 1, 9)
+'''
+##Home machine
+output_path = "C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\
+                  MNIST_Load\\Images\\test_image_"
+'''
+
+##tlab machine
+output_path = "/u/jc2/Desktop/test_image_"
+module.save_images(file_list[0], output_path, 10)
 
 
 
