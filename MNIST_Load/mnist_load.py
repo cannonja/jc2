85e024a7bb4c806e6ab0436f98480e1444515660
jc2 <jc2@21c10792-256f-4f31-8162-26c2349c2ac3>
2016-01-28 22:16:19 +0000

    Implemented save_images
    git-svn-id: https://projects.cecs.pdx.edu/svn/tlab/people/git/jc2/trunk@3866 21c10792-256f-4f31-8162-26c2349c2ac3

--- MNIST_Load/mnist_load.py
+++ MNIST_Load/mnist_load.py
@@ -4,11 +4,14 @@ from PIL import Image
 import numpy as np
 
 os.chdir("C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load")
-files = os.listdir()[2:]
+#files = os.listdir()[2:]
 
-file_name = files[0]
+#file_name = files[0]
+#output_path = "C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
 
-'''
+
+
+
 def print_meta(file_name):
     in_file = open(file_name, 'r+b')
     
@@ -27,29 +30,26 @@ def print_meta(file_name):
     print ("\n")
 
     in_file.close()
-'''
-
-in_file = open(file_name, 'r+b')
-in_file.seek(16)
-array = np.empty((28,28), 'uint8')
-images = np.zeros((28,28,10), 'uint8')
-path = "C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
-
-for i in range(0,10):
-
-    for j in range(0, 28):
-        row_data = in_file.read(28)
-        row_data = struct.unpack('>28B', row_data)
-        array[j] = row_data
-
-    images[:,:,i] = array
-
-    
-    im = Image.fromarray(images[:,:,i], mode='L')
-    fp = path + str(i) + ".png"
-    #fp = "test_image_" + str(i) + ".png"
-    im.save(fp)
-    
 
 
-in_file.close()
+def save_images(file_name, output_path, num_images, start=0):
+    array = np.empty((28,28), 'uint8') #Array to hold one image's data
+    images = [] #Empty list to hold multiple arrays (images)
+
+    in_file = open(file_name, 'r+b')
+
+    #Start at appropirate offset - 16 bytes for meta data
+    #Each image is (28 * 28) bytes
+    in_file.seek(16 + (start * 28 * 28)) 
+
+    for i in range(0, num_images):
+        for j in range(0, 28):
+            row_data = in_file.read(28)
+            row_data = struct.unpack('>28B', row_data)
+            array[j] = row_data
+        images.append(array)
+        im = Image.fromarray(images[i], mode='L')
+        fp = output_path + str(i + (start + 1)) + ".png"
+        im.save(fp)
+
+    in_file.close()
