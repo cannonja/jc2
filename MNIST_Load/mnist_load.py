00574d590b4afc71c8c3f5f9d8af69fe80e62565
jc2 <jc2@21c10792-256f-4f31-8162-26c2349c2ac3>
2016-01-28 22:16:18 +0000

    Worked out algo to read data and save as image
    git-svn-id: https://projects.cecs.pdx.edu/svn/tlab/people/git/jc2/trunk@3865 21c10792-256f-4f31-8162-26c2349c2ac3

--- MNIST_Load/mnist_load.py
+++ MNIST_Load/mnist_load.py
@@ -1,12 +1,14 @@
 import os
 import struct
+from PIL import Image
+import numpy as np
 
 os.chdir("C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load")
-files = os.listdir()[1:]
+files = os.listdir()[2:]
 
 file_name = files[0]
 
-
+'''
 def print_meta(file_name):
     in_file = open(file_name, 'r+b')
     
@@ -25,4 +27,29 @@ def print_meta(file_name):
     print ("\n")
 
     in_file.close()
+'''
 
+in_file = open(file_name, 'r+b')
+in_file.seek(16)
+array = np.empty((28,28), 'uint8')
+images = np.zeros((28,28,10), 'uint8')
+path = "C:\\Users\\Jack2\\Google Drive\\Grad School\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
+
+for i in range(0,10):
+
+    for j in range(0, 28):
+        row_data = in_file.read(28)
+        row_data = struct.unpack('>28B', row_data)
+        array[j] = row_data
+
+    images[:,:,i] = array
+
+    
+    im = Image.fromarray(images[:,:,i], mode='L')
+    fp = path + str(i) + ".png"
+    #fp = "test_image_" + str(i) + ".png"
+    im.save(fp)
+    
+
+
+in_file.close()
