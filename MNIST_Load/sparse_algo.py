01861b2479897d14e1df121de170bffb9199b4d3
cannonja <jack_cannon@live.com>
2016-02-04 21:37:48 -0800

    Implemented and tested normalize

--- MNIST_Load/sparse_algo.py
+++ MNIST_Load/sparse_algo.py
@@ -1,62 +1,44 @@
 import os
 import struct
+import sys
 from PIL import Image
 import numpy as np
 
-#Big laptop
-#os.chdir("C:\\Users\\Jack2\\Google Drive\\URMP\\jc2\\MNIST_Load")
+##Big laptop
+sys.path.append('C:\\Users\\Jack2\\Google Drive\\URMP\\jc2\\MNIST_Load')
+##Little laptop
+#sys.path.append('C:\\Users\\Jack\\Google Drive\\URMP\\jc2\\MNIST_Load')
+import mnist_load as mnist
 
-#Little laptop
-os.chdir("C:\\Users\\Jack\\Google Drive\\URMP\\jc2\\MNIST_Load")
-
-
-
-def print_meta(file_name):
-    in_file = open(file_name, 'r+b')
+#Function takes a 2-D numpy array and returns a tuple with a normalized 2-D numpy array
+#and the associated max and min used to normalize
+#Normalization method is (im_data[i,j] - min) / (max - min)
+def normalize(im_data):
+    normalized = np.zeros(im_data.shape)
+    dmax = np.amax(im_data)
+    dmin = np.amin(im_data)
     
-    if (file_name.find("image") != -1):
-        meta_data = in_file.read(16)
-        meta_data = struct.unpack('>4i', meta_data)
-        meta_labels = ("Magic number", "Number of images", "Rows", "Columns")
-    else:
-        meta_data = in_file.read(8)
-        meta_data = struct.unpack('>2i', meta_data)
-        meta_labels = ("Magic number", "Number of items")        
+    for i,j in zip(np.nditer(im_data), np.nditer(normalized, op_flags = ['readwrite'])):
+        j[...] = (i - dmin) / (dmax - dmin)
 
-    print ("File name:", in_file.name)
-    for (i,j) in zip(meta_labels, meta_data):
-        print (i, ": ", j, sep="")
-    print ("\n")
-
-    in_file.close()
+    return (normalized, dmax, dmin)
 
 
-def load_images(file_name, num_images, start=0):
-    images = [] #Empty list to hold 2-D numpy arrays containing image data
-    in_file = open(file_name, 'r+b') #input stream variable
+'''
+#Function takes 2-D numpy array and associated max/min, then returns un-normalized array
+def denormalize(norm, dmax, dmin):
+'''
 
-    #Start at appropirate offset - 16 bytes for meta data
-    #Each image is (28 * 28) bytes
-    in_file.seek(16 + (start * 28 * 28)) 
-
-    #Build image list element-wise where each element
-    #is a 2-D array of image data
-    for i in range(num_images):
-        array = np.empty((28,28), 'uint8') #Array to hold one image's data
-        for j in range(28):
-            row_data = in_file.read(28)
-            row_data = struct.unpack('>28B', row_data)
-            array[j] = row_data
-        images.append(array)
-
-    in_file.close()        
-    return images
-
-
-def save_images(image_list, output_path):
-    for i in range(len(image_list)):
-        im = Image.fromarray(image_list[i], mode='L')
-        fp = output_path + str(i + 1) + ".png"
-        im.save(fp)
-
+#Function takes list of 2-D numpy arrays representing images
+#Then flattens each image (converts to atom), then adds to dictionary
+#Function returns a dictionary where each atom is an image
+def build_dictionary(im_data):
+    
+    
+    
+    
+        
+    
+    
+    
     
--- Test/test_sparse_algo.py
+++ Test/test_sparse_algo.py
@@ -3,6 +3,47 @@ import os
 import sys
 from PIL import Image
 import numpy as np
+<<<<<<< HEAD
+
+##Big laptop
+sys.path.append('C:\\Users\\Jack2\\Google Drive\\URMP\\jc2\\MNIST_Load')
+##Little laptop
+#sys.path.append('C:\\Users\\Jack\\Google Drive\\URMP\\jc2\\MNIST_Load')
+import mnist_load as mnist
+import sparse_algo as sp
+
+
+##############################Built list of files to iterate through####################################################
+'''
+##Big laptop
+os.chdir("C:\\Users\\Jack2\\Google Drive\\URMP\\jc2\\MNIST_Load")
+##Little laptop
+#os.chdir("C:\\Users\\Jack\\Google Drive\\URMP\\jc2\\MNIST_Load")
+
+files = os.listdir()
+file_list = []
+for i in files:
+    if (i.find("idx") != -1):
+        file_list.append(i)
+'''
+
+
+##############################Test normalize############################################################################
+
+image_data = []
+image_data.append(np.array([[0,1,2],[3,4,5],[6,7,8]], dtype = 'int32'))
+image_data.append(np.array([[9,10,11],[12,13,14],[15,16,17]], dtype = 'int32'))
+
+print(type(image_data))
+print (image_data[0])
+print (image_data[1])
+normal, maximum, minimum  = sp.normalize(image_data[0])
+print (maximum, minimum)
+print (normal)
+
+
+
+=======
 ##Big laptop
 #sys.path.append('C:\\Users\\Jack2\\Google Drive\\URMP\\jc2\\MNIST_Load')
 
@@ -48,14 +89,25 @@ for name in (file_list):
 
 
 ##############################Check load_images and save_images#########################################################
+>>>>>>> 5b041cb3cc41196187a74da63c6ff80b2a3bbb7d
 
 ##Big laptop   
 #output_path = "C:\\Users\\Jack2\\Google Drive\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
 ##Little laptop   
+<<<<<<< HEAD
+#output_path = "C:\\Users\\Jack\\Google Drive\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
+
+#image_data = mnist.load_images(file_list[0], 5, 5)
+
+
+
+
+=======
 output_path = "C:\\Users\\Jack\\Google Drive\\URMP\\jc2\\MNIST_Load\\Images\\test_image_"
 
 image_data = mnist.load_images(file_list[0], 5, 5)
 mnist.save_images(image_data, output_path)
+>>>>>>> 5b041cb3cc41196187a74da63c6ff80b2a3bbb7d
 
 
 
