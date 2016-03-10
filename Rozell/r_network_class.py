659382a57a93ad1b8a76ff7454134b7e5cc8c3bf
cannonja <jack_cannon@live.com>
2016-03-09 18:59:21 -0800

    Implemented constructor, set_stimulus, and thresh

--- Rozell/r_network_class.py
+++ Rozell/r_network_class.py
@@ -3,6 +3,30 @@ import numpy as np
 class r_network:
     'Class to represent Rozell LCA network'
 
-    def __init__(self, D, s):
-        self.dictionary = np.asarray(
+    def __init__(self, D):
+        self.dictionary = D
+        self.s = None
+        self.b = None
+        self.a = None
+
+    #Takes the stimuls signal, sets s, then computes b according to Rozell
+    def set_stimulus(signal):
+        self.s = np.asarray(signal)
+        self.b = np.asarray(np.dot(np.transpose(D), self.s))
+
+    #Takes u, lamb, and t_type then returns a according to Rozell
+    #u is the internal state variable, lamb is lambda (threshold),
+    #and t_type is the soft or hard thresholding option ('S' or 'H')
+    def thresh(u, lamb, t_type):
+        if (u < lamb):
+            return 0
+        if (t_type == 'H'):
+            return u
+        if (t_type == 'S'):
+            return u - lamb
+
+    
+
+    
+        
 
