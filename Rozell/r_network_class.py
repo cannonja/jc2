bc7d6b3ab73a84c2ae35ad7cab72c0fc5c72c632
cannonja <jack_cannon@live.com>
2016-04-05 13:41:03 -0700

    Fixing tlab

--- Rozell/r_network_class.py
+++ Rozell/r_network_class.py
@@ -1,7 +1,13 @@
 import numpy as np
 import math
-import pandas
 
+###To do######
+'''
+-Implement methods to:
+    -Save image, reconstruction, and components
+    -Calculate and plot error function E(t)
+        -Implement Rozell's E(t) and Walt's E(t)
+'''
 
 class r_network:
     'Class to represent Rozell LCA network'
@@ -11,162 +17,79 @@ class r_network:
         self.s = None
         self.b = None
         self.a = None
-        self.lamb = None
-        self.tau = None
-        self.delta = None
-        self.u_stop = None
-        self.t_type = None
-
-    def scale(self, num):
-        self.s *= num
-        self.dictionary *= num
-        self.b = np.dot(np.transpose(self.dictionary), self.s)
-        
-
 
     #Takes the stimulus signal, sets s, then computes b according to Rozell
     def set_stimulus(self, signal):
         self.s = np.asarray(signal, dtype=float)
         self.b = np.dot(np.transpose(self.dictionary), self.s)
+        #print("set_stimulus\ns = ", self.s.shape, sum(self.s), "\nb = ",
+        #      self.b.shape, sum(self.b))
 
-
-    def set_lambda(self, lamb):
-        self.lamb = lamb
-
-    def set_tau(self, tau):    
-        self.tau = tau
-
-    def set_delta(self, delta):
-        self.delta = delta
-
-    def set_ustop(self, u_stop):
-        self.u_stop = u_stop
-
-    def set_ttype(self, t_type):
-        self.t_type = t_type
-
-    def set_parameters(self, lamb, tau, delta, u_stop, t_type):
-        self.set_lambda(lamb)
-        self.set_tau(tau)
-        self.set_delta(delta)
-        self.set_ustop(u_stop)
-        self.set_ttype(t_type)
-
-
-    #Takes u then returns a according to Rozell
+    #Takes u, lamb, and t_type then returns a according to Rozell
     #u is the internal state variable, lamb is lambda (threshold),
     #and t_type is the soft or hard thresholding option ('S' or 'H')
-    def thresh(self, u):
-        if (u < self.lamb):
+    def thresh(self, u, lamb, t_type):
+        if (u < lamb):
             return 0
-        if (self.t_type == 'H'):
+        if (t_type == 'H'):
             return u
-        if (self.t_type == 'S'):
-            return u - self.lamb
+        if (t_type == 'S'):
+            return u - lamb
 
-    def generate_sparse(self):
+    def return_sparse(self, lamb, tau, delta, u_stop, t_type):
         u = np.zeros(self.b.shape)
         self.a = u.copy()  #Initialize a by setting equal to u
-        self.scale(1/255)
         inhibit = np.dot(np.transpose(self.dictionary), self.dictionary)\
-                        - (np.eye(self.dictionary.shape[1]) / 255)
-        udot = (1/self.tau) * (self.b - u - np.dot(inhibit, self.a))
+                        - np.eye(self.dictionary.shape[1])
+        udot = (1/tau) * (self.b - u - np.dot(inhibit, self.a))
+
+        '''
+        print("b", self.b)
+        print("u", u)
+        print("a", self.a)
+        #print("b - u", (self.b - u))
+        #print("inhibit * a", np.dot(inhibit, self.a))
+        print("udot", udot)
+        '''
+        
+        
+        
         loop_flag = True
         
         #Generate vector self.a
+        #print(self.b)
         #debug = []
-        len_u = len(u)
+        num_iter = 0
         while (loop_flag):
-            u = u + (udot * self.delta)
+            #print("loop start....\n")
+            u = u + (udot * delta)
+            #print("u", u)
+            #print("b - u", (self.b - u))
+
             #Update a vector
             for i in range(len(self.a)):
-                self.a[i] = self.thresh(u[i])          
+                self.a[i] = self.thresh(u[i], lamb, t_type)
+            #print("a", self.a)
+            #print("inhibit * a", np.dot(inhibit, self.a))            
             
-            udot = (1/self.tau) * (self.b - u - np.dot(inhibit, self.a))            
-            udot_length = math.sqrt(np.dot(udot,udot))
-            if ((udot_length / len_u) < (self.u_stop / len_u)):
+            udot = (1/tau) * (self.b - u - np.dot(inhibit, self.a))            
+            #print("udot", udot)
+
+            udot_length = math.sqrt(np.dot(np.transpose(udot),udot))
+            num_iter += 1
+            if (udot_length < u_stop):
                 loop_flag = False
-
+                print(num_iter)
+            
+            
+            #print("\nloop end.....\n")
             #debug.append({ 'a': self.a.copy(), 'u': u.copy(), 'udot': ... })
-
-        self.scale(255)
         '''
         df = pandas.DataFrame(debug)
         print df.to_string()
         '''
-
-    #Returns 1-D numpy array containing E(t), the left-hand operand of E(t),
-    #and the right-hand operand of E(t).
-    #That is the operands of the right-hand side of the error equation
-    def return_error(self):
-        stim = self.s
-        recon = np.dot(self.dictionary, self.a)
-        resid = stim - recon
-        a = 0.5 * math.sqrt(np.dot(resid, resid))
-        b = None
-        sparsity = len(self.a[self.a > 0]) / len(self.a)
-        norm1 = sum(abs(self.a))
-        cost = 0
-
-        if (self.t_type == 'S'):
-            cost = norm1
-        elif (norm1 > self.lamb):
-            cost = self.lamb / 2
-
-        b = self.lamb * cost        
-        #error = np.array([[self.lamb, (a + b), a, b, sparsity]])
-        error = np.array([[(a + b), a, b, sparsity]])
-
-        return error
-
-    #This method returns two arrays:
-    #coefficients consists of the active coefficients
-    #rfileds consists of the respective dictionary elements
-    def get_rfields(self):
-        indices = np.where(self.a > 0)
-        rfields = self.dictionary[:, indices]
-        coefficients = self.a[indices]
-
-        return (coefficients, rfields)
-
-    #This method takes a single lambda (as a list) or an array
-    #of lambdas, then returns an error table where each row represents
-    #different error measures at a particular lambda
-    def error_table(self, lambdas):
-        df = pandas.DataFrame() #DataFrame used for error table
-
-
-
-
-
-        display = []  #List to hold rows of image data for grid (rfields scaled)
-        display2 = [] #Unscaled rfields
-
-        #For each value of lambda, set lambda and run Rozell on the given image
-        for j in lambdas:
-            display_row = []   #List to hold one row of image data (for display)
-            display_row2 = []  #For display2
-            self.set_lambda(j)
-            self.generate_sparse()  #Calculate sparse code        
-            ##Add row of error data to error table
-            row = pandas.DataFrame(self.return_error())
-            df = df.append(row)
-            ##Add list of dictionary elements scaled by coefficients to list
-            ##Also adding recostruction to list
-            indices = np.flatnonzero(self.a)
-            coeff = self.a[indices]
-            rfields = self.dictionary[:, indices]
-            reconstruction = np.dot(rfields, coeff).reshape((28,28))
-            display_row.append(reconstruction)
-            display_row2.append(reconstruction)
-            for k in range(len(coeff)):
-                display_row.append((coeff[k] * rfields[:, k]).reshape((28,28)))
-                display_row2.append(rfields[:, k].reshape((28,28)))
-            display.append(display_row)
-            display2.append(display_row2)
-        
-
-        
+ 
+        return self.a
             
 
         
--- Test/test_r_network.py
+++ Test/test_r_network.py
@@ -4,8 +4,6 @@ import sys
 from PIL import Image
 import numpy as np
 import socket
-import pandas
-import matplotlib.pyplot as plt
 
 machine = socket.gethostname()
 if (machine == 'Jack-PC'):
@@ -46,10 +44,10 @@ print (u)
 
 ##############################Test return_sparse##########################################################################################
 
-lamb = .1
-tau = 10
+lamb = 0.01
+tau = 100
 delta = 0.001
-u_stop = 10
+u_stop = 100000
 t_type = 'H'
 num_images = 1
 
@@ -66,16 +64,9 @@ D = np.ones((5,3))
 D[:,0] = signal
 D[:,1] = (7,0,32,5,7)
 D[:,2] = (21,5,8,9,23)
-
-#Test scaling
-network = lca.r_network(D)
-network.set_stimulus(signal)
-network.set_scale(255)
-print(signal)
-print(network.dictionary)
 '''
 
-#Build list of  to iterate through - choose first file (t10k-images.idx3-ubyte)
+#Build list of files to iterate through - choose first file (t10k-images.idx3-ubyte)
 
 files = os.listdir()
 file_list = []
@@ -85,100 +76,33 @@ for i in files:
 
 
 #Load MNIST dictionary and signal
-#image_file = file_list[0]
-image_file = 't10k-images.idx3-ubyte'
-signal_data = mnist.load_images(image_file, num_images, 6000)
-#Insert stimulus in dictionary
-#dict_data = mnist.load_images(image_file, 49, 20)
-#D = np.append(signal_data[0].flatten().reshape(784,1), sp.build_dictionary(dict_data), axis = 1)
-##Use regular dictionary
-dict_data = mnist.load_images(image_file, 50)
+signal_data = mnist.load_images(file_list[0], num_images)
+
+dict_data = mnist.load_images(file_list[0], 50, 20)
+
+
 D = sp.build_dictionary(dict_data)
+
+
 
 #Run Rozell and generate sparse code
 network = lca.r_network(D)
-network.set_parameters(lamb, tau, delta, u_stop, t_type)
-#error_names = ['Lambda', 'E(t)', 'Resid', 'Cost', 'Sparsity']
-error_names = ['E(t)', 'Resid', 'Cost', 'Sparsity']
-lambdas = np.arange(0.1, 2.1, 0.1)
-#lambdas = [0.1]
 
-#For each image, run Rozell, generate sparse code, error table and image grid
 for i in range(num_images):
     signal = signal_data[i].flatten()
+    #im = Image.fromarray(signal.reshape((28,28)))
+    #im.show()
     network.set_stimulus(signal)
-    #error = network.error_table(lambdas)
-    
-    error = pandas.DataFrame() #DataFrame used for error table
-    display = []  #List to hold rows of image data for grid (rfields scaled)
-    display2 = [] #Unscaled rfields
-    #For each value of lambda, set lambda and run Rozell on the given image
-    for j in lambdas:
-        display_row = []   #List to hold one row of image data (for display)
-        display_row2 = []  #For display2
-        network.set_lambda(j)
-        network.generate_sparse()  #Calculate sparse code
-        ##Add row of error data to error table
-        row = pandas.DataFrame(network.return_error())
-        error = error.append(row)
-        ##Add list of dictionary elements scaled by coefficients to list
-        ##Also adding recostruction to list
-        indices = np.flatnonzero(network.a)
-        coeff = network.a[indices]
-        rfields = network.dictionary[:, indices]
-        reconstruction = np.dot(rfields, coeff).reshape((28,28))
-        display_row.append(reconstruction)
-        display_row2.append(reconstruction)
-        for k in range(len(coeff)):
-            display_row.append((coeff[k] * rfields[:, k]).reshape((28,28)))
-            display_row2.append(rfields[:, k].reshape((28,28)))
-        display.append(display_row)
-        display2.append(display_row2)
-    
+    code = network.return_sparse(lamb, tau, delta, u_stop, t_type)
+    print (code)
 
-    ##Add column names to error table
-    error.columns = error_names
-    error.set_index(lambdas)
-    ##Get max number of components for display grid dimensions
-    biggest = 0
-    for j in display:
-        if (len(j) > biggest):
-            biggest = len(j)
-    ##Allocate pixels for display grid and display grid2
-    grid = np.full((28 * len(lambdas), 28 * (biggest + 1)), 255.)
-    grid2 = np.full((28 * len(lambdas), 28 * (biggest + 1)), 255.)
+orig = network.s.reshape((28,28))
+recon = np.dot(D, network.a).reshape((28,28))
+im_orig = Image.fromarray(orig)
+im_recon = Image.fromarray(recon)
 
-    ##Fill display grid with image data
-    ##Iterate over row - for each row, add columns
-    for j in range(len(lambdas)):
-        rows = slice(j*28, (j+1)*28)
-        #Original image
-        grid[rows, :28] = network.s.reshape((28,28))
-        grid2[rows, :28] = network.s.reshape((28,28))
-        #Reconstruction and components
-        for k in range(len(display[j])):
-            cols = slice((k+1)*28, (k+2)*28)
-            grid[rows, cols] = display[j][k]
-            grid2[rows, cols] = display2[j][k]
-
-    print(error)
-
-
-
-    plt.subplot(211)
-    plt.plot(lambdas, error['E(t)'], 'r')
-    #b = plt.plot(error['Lambda'], error['Resid'], 'b')
-    #x2 = plt.twinx()
-    plt.subplot(212)
-    plt.plot(lambdas, error['Sparsity'], 'c')
-    plt.show()
-    
-    
-    im_grid = Image.fromarray(grid)
-    im_grid2 = Image.fromarray(grid2)
-    im_grid.show()
-    im_grid2.show()
-    
+im_orig.show()
+im_recon.show()
 
 
 
