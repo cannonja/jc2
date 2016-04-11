8236dcd37f02a8e36b2d5888295931bb98075042
cannonja <jack_cannon@live.com>
2016-04-11 01:44:36 -0700

    Started layout for learning algo

---
+++ Test/test_learning.py
@@ -0,0 +1,88 @@
+import imp
+import os
+import sys
+from PIL import Image
+import numpy as np
+import socket
+import pandas
+import matplotlib.pyplot as plt
+
+machine = socket.gethostname()
+if (machine == 'Jack-PC'):
+    #Big laptop
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell')
+    os.chdir('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+elif (machine == 'Tab'):
+    #Little laptop
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Rozell')
+    os.chdir('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+else:
+    #PSU machines (linux lab)
+    base = os.path.expanduser('~/dev/jc2')
+    sys.path.append(os.path.join(base, 'MNIST_Load'))
+    sys.path.append(os.path.join(base, 'Rozell'))
+    os.chdir(os.path.join(base, 'MNIST_Load'))
+
+
+import mnist_load as mnist
+import sparse_algo as sp
+import r_network_class as lca
+
+
+################### Set parameters ##############################################################
+lamb = .1
+tau = 10
+delta = 0.001
+u_stop = .01
+t_type = 'S'
+num_images = 1
+
+################### Load dictionary and training set from MNIST #################################
+################### Dictionary = first 50 images, training set = last 59950 images ##############
+image_file = 'train-images.idx3-ubyte'
+dict_data = mnist.load_images(image_file, 50)
+training_data = mnist.load_images(image_file, 59950, 49)
+D = sp.build_dictionary(dict_data)
+
+
+'''
+#Run Rozell and generate sparse code
+network = lca.r_network(D)
+network.set_parameters(lamb, tau, delta, u_stop, t_type)
+
+
+
+
+#For each image, run Rozell then generate error table and image grid
+for i in range(num_images):
+    #Get number of rows for error table and image grid
+    #Then set stimulus for Rozell
+    rows = len(lambdas)
+    signal = signal_data[i].flatten()
+    network.set_stimulus(signal)
+    
+    #Run Rozell and get error and image grid data
+    error, im1, im2 = network.reconstruct(lambdas)
+
+    #Add error table characteristics
+    #Plot both E(t) and Sparsity vs. lambdas
+    #E(t) on top plot and Sparsity on bottom
+    error.columns = error_names
+    error.set_index(lambdas)
+    print(error)
+    plt.subplot(211)
+    plt.plot(lambdas, error['E(t)'], 'r')
+    plt.subplot(212)
+    plt.plot(lambdas, error['Sparsity'], 'c')
+    plt.show()
+    
+    #Generate and show grid images
+    grid = network.fill_grid(rows, im1)
+    grid2 = network.fill_grid(rows, im2)    
+    im_grid = Image.fromarray(grid)
+    im_grid2 = Image.fromarray(grid2)
+    im_grid.show()
+    im_grid2.show()
+'''
