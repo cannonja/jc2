ade755a1c9cefcb94055d51f08a205ebe3a40766
cannonja <jack_cannon@live.com>
2016-04-30 02:30:02 -0700

    Implemented load_labels - begin testing

--- MNIST_Load/mnist_load.py
+++ MNIST_Load/mnist_load.py
@@ -46,6 +46,22 @@ def load_images(file_name, num_images, start=0):
     return images
 
 
+def load_labels(file_name, num_images, start=0):
+    labels = [] #Empty list to hold integers corresponding to image labels
+    in_file = open(file_name, 'r+b') #input stream variable
+
+    #Start at appropirate offset - 8 bytes for meta data
+    #Each image is (28 * 28) bytes
+    in_file.seek(8 + start) 
+
+    #Build label list element-wise where each element
+    #is an integer cooresponding to a label of an image
+    for i in range(num_images):
+        labels.append(struct.unpack('>1B', in_file.read(1)))
+    in_file.close()        
+    return labels
+
+
 def save_images(image_list, output_path):
     for i in range(len(image_list)):
         im = Image.fromarray(image_list[i], mode='L')
