3b70c345e10c5c9020cfe8b5c1b24d8bb72cfe6f
cannonja <jack_cannon@live.com>
2016-05-02 21:57:48 -0700

    Started coding backprop

--- Test/test_learning2.py
+++ Test/test_learning2.py
@@ -16,10 +16,6 @@ if (machine == 'Jack-PC'):
     sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell')
     os.chdir('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
     file_path = 'C:\\Users\\Jack2\\Desktop'
-    dict1_path = file_path + '/orig_dict.png'
-    dict2_path = file_path + '/trained_dict.png'
-    write_path = file_path + '/resid_data.csv'
-    plot_path = file_path + '/resid_plot.png'
 elif (machine == 'Tab'):
     #Little laptop
     sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
@@ -33,12 +29,6 @@ else:
     sys.path.append(os.path.join(base1, 'Rozell'))
     os.chdir(os.path.join(base1, 'MNIST_Load'))
     file_path = base2
-    dict1_path = file_path + '/orig_dict'
-    dict2_path = file_path + '/trained_dict'
-    write_path = file_path + '/resid_data'
-    plot_path = file_path + '/resid_plot'
-
-
 
 import mnist_load as mnist
 import sparse_algo as sp
@@ -53,19 +43,18 @@ u_stop = .01
 t_type = 'S'
 alpha = 0.1
 
-################### Load dictionary from first 50 MNIST images ##################################
-################### Load training set from last 59950 MNIST images ##############################
-num_rfields = 50
-num_images = 25 #60000 - num_rfields
+################### Load MNIST image and label data #############################################
+num_images = 20
+start_pos = 0
 image_file = 'train-images.idx3-ubyte'
-mnist.print_meta(image_file)
-dict_data = mnist.load_images(image_file, num_rfields)
-training_data = mnist.load_images(image_file, num_images, num_rfields)
+label_file = 'train-labels.idx1-ubyte'
+image_data = mnist.load_images(image_file, num_images, start_pos)
+label_data = mnist.load_labels(label_file, num_images, start_pos)
 
 #Initialize network dictionary and parameters
-D = sp.build_dictionary(dict_data)
-network = lca.r_network(D)
-network.set_parameters(lamb, tau, delta, u_stop, t_type)
+D = 
+rozell = lca.r_network(D)
+rozell.set_parameters(lamb, tau, delta, u_stop, t_type)
 
 
 ################### Run each training image through network #######################################
