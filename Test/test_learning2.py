8a35e8a563de4fd1a3d81a068725538de5cab5c0
cannonja <jack_cannon@live.com>
2016-05-04 01:07:39 -0700

    Ran program and generate results for different runs.  Still troubleshooting

--- Test/test_learning2.py
+++ Test/test_learning2.py
@@ -33,6 +33,9 @@ else:
     os.chdir(os.path.join(base1, 'MNIST_Load'))
     file_path = base2
     dict_path = base1 + '/Classify/trained_data'
+    plot_path = base1 + '/Classify/RMSE_plot'
+    accuracy_path = base1 + '/Classify/Accuracy_plot'
+    weight_path = base1 + '/Classify/weights.csv'
 
 import mnist_load as mnist
 import sparse_algo as sp
@@ -41,7 +44,7 @@ import classify
 
 
 ################### Set parameters ##############################################################
-lamb = .1
+lamb = 1.5
 tau = 10
 delta = 0.001
 u_stop = .01
@@ -49,8 +52,8 @@ t_type = 'S'
 alpha = 0.1
 
 ################### Load MNIST image and label data #############################################
-num_images  = 200
-start_pos = 0
+num_images  = 1500
+start_pos = 10000
 image_file = 'train-images.idx3-ubyte'
 label_file = 'train-labels.idx1-ubyte'
 image_data = mnist.load_images(image_file, num_images, start_pos)
@@ -61,13 +64,46 @@ dict_data = pd.read_csv(dict_path, header = None)
 rozell = lca.r_network(dict_data.values)
 rozell.set_parameters(lamb, tau, delta, u_stop, t_type)
 
+'''
 ################### Initialize random  matrix of weights for mapping ############################
-################### sparse code to output layer.  Then forward prop phase #######################
+################### sparse code to output layer.  Then train network #### #######################
 weights = np.random.rand(10, 51)    #10 nodes in layer j+1 and 50 nodes in layer j
-learn_rate = 0.5
+learn_rate = 0.1
 error_plot = np.array([])
-correct = 0
 for i, j in zip(image_data, label_data):
+    #Run Rozell and forwardprop
+    rozell.set_stimulus(i.flatten())
+    sparse_code = np.append(rozell.generate_sparse(), 1) #Add bias term
+    _, error, D = classify.forward_prop(weights, sparse_code, j)
+
+    #Store error - represented as quadratic error: 0.5*(error)^2
+    MSE = np.dot(np.transpose(error), 0.5 * error) / error.shape[0]
+    RMSE = float(np.sqrt(MSE))
+    error_plot = np.append(error_plot, RMSE)
+
+    #Run backprop
+    update = classify.back_prop(D, sparse_code, error, learn_rate)
+    weights += update
+
+#Save weights and plot RMSE
+df = pd.DataFrame(weights)
+df.to_csv(weight_path, header=False, index=False)
+plt.plot(error_plot)
+plt.xlabel('Image Number')
+plt.title('RMSE During Backprop')
+plt.savefig(plot_path)
+plt.show()
+
+'''
+#################### Read in weights and test network #########################################
+df = pd.read_csv(weight_path, header= None, names=None)
+weights = df.values
+correct = 0
+count = 0
+error_plot = np.array([])
+accuracy_plot = np.array([])
+for i, j in zip(image_data, label_data):
+    count += 1
     #Run Rozell and forwardprop
     rozell.set_stimulus(i.flatten())
     sparse_code = np.append(rozell.generate_sparse(), 1) #Add bias term
@@ -79,18 +115,24 @@ for i, j in zip(image_data, label_data):
     MSE = np.dot(np.transpose(error), 0.5 * error) / error.shape[0]
     RMSE = float(np.sqrt(MSE))
     error_plot = np.append(error_plot, RMSE)
-    #Run backprop
-    update = classify.back_prop(D, sparse_code, error, learn_rate)
-    weights += update
+    accuracy_plot = np.append(accuracy_plot, (correct / count))
 
+accuracy = correct / num_images
 
-print (correct / num_images)
+#Plot RMSE
+plt.figure()
 plt.plot(error_plot)
+plt.xlabel('Image Number')
+plt.title('RMSE During Testing - accuracy = ' + str(accuracy))
+plt.savefig(plot_path + '2')
+
+#Plot accuracy
+plt.figure()
+plt.plot(accuracy_plot)
+plt.xlabel('Image Number')
+plt.title('Accuracy During Testing')
+plt.savefig(accuracy_path)
 plt.show()
-
-
-
-
 
 
 
