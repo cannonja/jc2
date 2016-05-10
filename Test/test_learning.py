Starting to fix scaling issues

Test/test_learning.py
@@ -55,10 +55,10 @@ import r_network_class as lca
 
 
 ################### Set parameters ##############################################################
-lamb = 1
-tau = 10
-delta = 0.001
-u_stop = 0.01
+lamb = 1.0
+tau = 10.0
+delta = 0.01
+u_stop = 0.001
 t_type = 'S'
 alpha = 0.2
 
@@ -69,10 +69,10 @@ win2 = 500 #Window for mov avg 2
 ################### Load dictionary from first 50 MNIST images ##################################
 ################### Load training set from last 59950 MNIST images ##############################
 num_rfields = 50
-num_images =  5000       #60000 - num_rfields
+num_images =  1000      #60000 - num_rfields
 image_file = 't10k-images.idx3-ubyte'  #'train-images.idx3-ubyte'
-dict_data = mnist.load_images(image_file, num_rfields, 1)
-training_data = mnist.load_images(image_file, num_images, 51)
+dict_data = mnist.load_images(image_file, num_rfields)
+training_data = mnist.load_images(image_file, num_images, 50)
 
 #Initialize network dictionary and parameters
 D = sp.build_dictionary(dict_data)
@@ -97,7 +97,7 @@ resid_plot = np.zeros((num_images))
 #Train dictionary as each image is run through network
 #Store length of residual vector in resid_plot array
 for i in range(num_images):
-    if (((i + 1) % 1000) == 0):
+    if (((i + 1) % 100) == 0):
         print("Image ",i + 1)
     stimulus = training_data[i].flatten()
     network.set_stimulus(stimulus, True)