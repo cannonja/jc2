ccca0619237b4accdf140a134608b5d4dfe8994a
cannonja <jack_cannon@live.com>
2016-06-05 00:14:18 -0700

    Started natural image sparse code project

--- MNIST_Load/sparse_algo.py
+++ MNIST_Load/sparse_algo.py
@@ -30,7 +30,7 @@ def denormalize(norm, dmax, dmin):
 #Then flattens each image (converts to atom) and adds to dictionary
 #Function returns a dictionary where each atom is an image
 def build_dictionary(im_data):
-    rows, cols = im_data[0].shape
+    rows, cols, _ = im_data[0].shape
     dictionary = np.zeros(((rows * cols), len(im_data)))
     
     for i in range(len(im_data)):
