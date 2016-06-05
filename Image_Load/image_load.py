5f1799b8fc43f5fe3763e5346e96f1cecdd372f7
cannonja <jack_cannon@live.com>
2016-06-05 00:31:53 -0700

    Started using image_load to experiment with patching function

--- Image_Load/image_load.py
+++ Image_Load/image_load.py
@@ -1,11 +1,66 @@
-##Didn't flatten the array to 1D
+import imp
+import os
+import sys
+from PIL import Image
+import numpy as np
+import socket
+import pandas
+import matplotlib.pyplot as plt
+import time
+import datetime
+import pdb
+
+
+machine = socket.gethostname()
+if (machine == 'Jack-PC'):
+    #Big laptop
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell')
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Image_Class')
+    os.chdir('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    file_path = 'C:\\Users\\Jack2\\Desktop'
+    dict1_path = file_path + '/orig_dict.png'
+    dict2_path = file_path + '/trained_dict.png'
+    dict3_path = file_path + '/trained_data.csv'
+    write_path = file_path + '/resid_data.csv'
+    plot_path = file_path + '/resid_plot2.png'
+    nat_path = 'C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\Natural_images'
+elif (machine == 'Tab'):
+    #Little laptop
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Rozell')
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Image_Class')
+    os.chdir('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    file_path = 'C:\\Users\\Jack\\Desktop'
+    dict1_path = file_path + '/orig_dict.png'
+    dict2_path = file_path + '/trained_dict.png'
+    dict3_path = file_path + '/trained_data.csv'
+    write_path = file_path + '/resid_data.csv'
+    plot_path = file_path + '/resid_plot.png'
+else:
+    #PSU machines (linux lab)
+    base1 = os.path.expanduser('~/dev/jc2')
+    base2 = os.path.expanduser('~/Desktop')
+    sys.path.append(os.path.join(base1, 'MNIST_Load'))
+    sys.path.append(os.path.join(base1, 'Rozell'))
+    sys.path.append(os.path.join(base1, 'Image_Class'))
+    os.chdir(os.path.join(base1, 'MNIST_Load'))
+    file_path = base1 + '/Test/DB Classifier/Overnight run'
+    dict1_path = file_path + '/orig_dict.png'
+    dict2_path = file_path + '/trained_dict.png'
+    dict3_path = file_path + '/trained_data.csv'
+    write_path = file_path + '/resid_data.csv'
+    plot_path = file_path + '/resid_plot.png'
 
 from PIL import Image
 import numpy as np
 
 ##Read image and convert to numpy array
-im = Image.open("Lenna.png")
+im = Image.open(nat_path + '\\Lenna.png')
 array = np.asarray(im)
+print (array.shape[0] / 8., array.shape[1] / 8.)
+
+'''
 array.flags.writeable = True
 
 ##Set RGB to zero
@@ -13,12 +68,10 @@ array[:,:,0] = 0   #R
 #array[:,:,1] = 0  #G
 array[:,:,2] = 0   #B
 
-#Convert to floating point bounded by [0,1]
-#Will not output as floating point
-#array = array / array.max()
 
 im2 = Image.fromarray(array)
-im2.save("Lenna2.png")
+im2.save(nat_path + '\\Lenna2.png')
+'''
 
 
 
