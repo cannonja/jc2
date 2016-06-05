389c87327014e0e2b1f05ddc580736a29cde9aab
cannonja <jack_cannon@live.com>
2016-06-05 00:19:29 -0700

    Removed garbage

--- Image_Class/image_class.py
+++ Image_Class/image_class.py
@@ -42,38 +42,4 @@ class image_class:
         #self.im = Image.fromarray(self.data)
         self.im.save(new_path)
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-##Read image and convert to numpy array
-im = Image.open("Lenna.png")
-array = np.asarray(im)
-array.flags.writeable = True
-
-##Set RGB to zero
-array[:,:,0] = 0   #R
-#array[:,:,1] = 0  #G
-array[:,:,2] = 0   #B
-
-#Convert to floating point bounded by [0,1]
-#Will not output as floating point
-#array = array / array.max()
-
-im2 = Image.fromarray(array)
-im2.save("Lenna2.png")
-'''
 
