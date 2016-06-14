4a6f694e00c961b8d4062f279e2ec81e444bc49a
cannonja <jack_cannon@live.com>
2016-06-14 05:28:00 -0700

    Tried running on big laptop - installing python 2

--- Rozell/Walt_code/walt_compare.py
+++ Rozell/Walt_code/walt_compare.py
@@ -2,7 +2,55 @@ import numpy as np
 import mr
 from mr.unsupervised import Lca
 
+
+machine = socket.gethostname()
+if (machine == 'Jack-PC'):
+    #Big laptop
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell')
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Image_Class')
+    os.chdir('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    file_path = 'C:\\Users\\Jack2\\Desktop'
+    dict1_path = file_path + '/orig_dict.png'
+    dict2_path = file_path + '/trained_dict.png'
+    dict3_path = file_path + '/trained_data.csv'
+    write_path = file_path + '/resid_data.csv'
+    plot_path = file_path + '/resid_plot2.png'
+    nat_path = 'C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\Natural_images'
+elif (machine == 'Tab'):
+    #Little laptop
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Rozell')
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Image_Class')
+    os.chdir('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\MNIST_Load')
+    file_path = 'C:\\Users\\Jack\\Desktop'
+    dict1_path = file_path + '/orig_dict.png'
+    dict2_path = file_path + '/trained_dict.png'
+    dict3_path = file_path + '/trained_data.csv'
+    write_path = file_path + '/resid_data.csv'
+    plot_path = file_path + '/resid_plot.png'
+else:
+    #PSU machines (linux lab)
+    base1 = os.path.expanduser('~/dev/jc2')
+    base2 = os.path.expanduser('~/Desktop')
+    sys.path.append(os.path.join(base1, 'MNIST_Load'))
+    sys.path.append(os.path.join(base1, 'Rozell'))
+    sys.path.append(os.path.join(base1, 'Image_Class'))
+    os.chdir(os.path.join(base1, 'MNIST_Load'))
+    file_path = base1 + '/Test/DB Classifier/Overnight run'
+    dict1_path = file_path + '/orig_dict.png'
+    dict2_path = file_path + '/trained_dict.png'
+    dict3_path = file_path + '/trained_data.csv'
+    write_path = file_path + '/resid_data.csv'
+    plot_path = file_path + '/resid_plot.png'
+    nat_path = base1 + '/Rozell/Natural_images'
+
+
+import mnist_load as mnist
+
+'''
 net = Lca(50)
 net.fit(X)
 print (net.score(X))
+'''
 
