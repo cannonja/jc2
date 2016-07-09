112925ac2c7458fb3b654738fc884636265bce60
cannonja <jack_cannon@live.com>
2016-07-09 13:56:21 -0700

    Started test script

---
+++ Rozell/Walt_code/mr/unsupervised/test/test_sklearnModelBase.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class test_constructor(unittest.TestCase):
+    
+    
+    
+    def test_get_params():
+        
+        
+    def test_set_params():
+        
+    
+    
+    
+
