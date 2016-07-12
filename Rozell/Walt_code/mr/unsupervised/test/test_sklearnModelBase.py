00cd888a7c83418d5bbdf45c44a90bfe7591ab93
cannonja <jack_cannon@live.com>
2016-07-12 00:16:40 -0700

    Trying to get test script to run

--- Rozell/Walt_code/mr/unsupervised/test/test_sklearnModelBase.py
+++ Rozell/Walt_code/mr/unsupervised/test/test_sklearnModelBase.py
@@ -8,14 +8,19 @@ if (machine == 'Jack-PC'):
     #Big laptop
     sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
                      'Walt_code\\mr')
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
+                     'Walt_code')
 elif (machine == 'Tab'):
     #Little laptop
-    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
                      'Walt_code\\mr')
+    sys.path.append('C:\\Users\\Jack\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
+                     'Walt_code')
 else:
     #PSU machines (linux lab)
     base1 = os.path.expanduser('~/dev/jc2/Rozell/Walt_code')
     sys.path.append(os.path.join(base1, 'mr'))
+    sys.path.append(os.path.join(base1))
 
 import pyximport
 pyximport.install()    
