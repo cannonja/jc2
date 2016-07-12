d16166f724bc67ca9ca4a4f84f264a99e4b15193
cannonja <jack_cannon@live.com>
2016-07-11 23:50:03 -0700

    Set up for first test run - intentional fail.  Doesn't work on laptop (vcvarsall.bat error)

--- Rozell/Walt_code/mr/unsupervised/test/test_sklearnModelBase.py
+++ Rozell/Walt_code/mr/unsupervised/test/test_sklearnModelBase.py
@@ -1,14 +1,43 @@
 import unittest
+import socket
+import sys
+import os
+
+machine = socket.gethostname()
+if (machine == 'Jack-PC'):
+    #Big laptop
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
+                     'Walt_code\\mr')
+elif (machine == 'Tab'):
+    #Little laptop
+    sys.path.append('C:\\Users\\Jack2\\Desktop\\Git_Repos\\jc2\\Rozell\\' + \
+                     'Walt_code\\mr')
+else:
+    #PSU machines (linux lab)
+    base1 = os.path.expanduser('~/dev/jc2/Rozell/Walt_code')
+    sys.path.append(os.path.join(base1, 'mr'))
+
+import pyximport
+pyximport.install()    
+import modelBase
 
 
-class test_constructor(unittest.TestCase):
+
+class test_get_params(unittest.TestCase):
     
-    
-    
-    def test_get_params():
+    def test_fail(self):    
+        self.failUnless(False)
         
-        
-    def test_set_params():
+
+if __name__ == '__main__':
+    unittest.main()
+   
+    
+
+
+#class test_set_params(unittest.TestCase):
+
+
         
     
     
