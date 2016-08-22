d91033a91a5f9117fd9164a3c95fbab03334e372
cannonja <jack_cannon@live.com>
2016-08-22 02:10:21 -0700

    Successfully ran - 70%

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -1,19 +1,24 @@
 from mr.datasets.mnist import MnistDataset
 from mr.learn.scaffold import Scaffold
 from mr.learn.convolve import ConvolveLayer
+from mr.learn.convolve import PoolLayer
 from mr.learn.unsupervised.lca import Lca
 from mr.learn.supervised.perceptron import Perceptron
 
 ## Load images
-nload = 1000
-train, test, vp = MnistDataset(nload).split(nload * 1 // 7)
+nload = 15000
+#train, test, vp = MnistDataset(nload).split(nload * 1 // 7)
+train, test, vp = MnistDataset(nload).split(10000)
 
 ## Set up model
 print ("Building model")
 model = Scaffold()
 c = ConvolveLayer(layer = Lca(15), visualParams = vp, convSize = 7,
             convStride = 3)
+c._init(len(train[0][0]), None)
 model.layer(c)
+p = PoolLayer(visualParams = c.visualParams)
+model.layer(p)
 model.layer(Perceptron())
 
 ## Train and test model
