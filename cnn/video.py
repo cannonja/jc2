8a4ba1509ed03d9dce884b9a489153075695fb60
cannonja <jack_cannon@live.com>
2016-08-23 01:30:10 -0700

    First video class

--- cnn/video.py
+++ cnn/video.py
@@ -1,7 +1,26 @@
-import skvideo.io
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-video = 'Neovision2-Training-Tower-001.mpg'
 
-vdata = skvideo.io.vread(video)
+class Video:
+    
+    def __init__(self, folder):
+        prior_dir = os.getcwd()
+        os.chdir(folder)        
+        frames = os.listdir(folder)
+        self.data = []
+        
+        for i in range(len(frames)):
+            im = Image.open(frames[i])
+            self.data.append(np.array(im.getdata(), np.uint8).reshape(im.size[0], 
+                             im.size[1], 3))
+        os.chdir(prior_dir)
+        
+        
+        
+        
+    
 
-print (type(vdata[0]), vdata[0].shape)
+
