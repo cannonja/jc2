c45883b100d72606208a241d1746bbc9b110b1a8
cannonja <jack_cannon@live.com>
2016-08-23 02:08:40 -0700

    Finished simple video class

--- cnn/video.py
+++ cnn/video.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 
 
@@ -10,12 +9,20 @@ class Video:
         prior_dir = os.getcwd()
         os.chdir(folder)        
         frames = os.listdir(folder)
-        self.data = []
+        frame_list = []
         
+        ## Put frames in list first, then flatten and store in ndarray
         for i in range(len(frames)):
             im = Image.open(frames[i])
-            self.data.append(np.array(im.getdata(), np.uint8).reshape(im.size[0], 
+            frame_list.append(np.array(im.getdata(), np.uint8).reshape(im.size[0], 
                              im.size[1], 3))
+                             
+        ## Store in ndarray as an attribute
+        tot_pixels = np.prod(frame_list[0].shape)
+        self.data = np.array(frame_list[0].flatten(), np.uint8).reshape(tot_pixels, 1)
+        for i in range(1, len(frame_list)):
+            self.data = np.hstack((self.data, np.array(frame_list[i].flatten(), 
+                                    np.uint8).reshape(tot_pixels, 1)))
         os.chdir(prior_dir)
         
         
