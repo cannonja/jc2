d210044014c7be7bdb49b9aa215f6ba826a7f3c0
cannonja <jack_cannon@live.com>
2016-08-25 22:06:43 -0700

    Using script to debug ImageDataset or whatever else is causing the problem (__init__ fails on last line)

--- Test/test_tower.py
+++ Test/test_tower.py
@@ -1,16 +1,44 @@
 import numpy as np
 import pandas as pd
+import os
 from mr.datasets.tower import StanfordTower as st
+from mr.datasets.common import ImageSet
+from mr.datasets.imageDataset import ImageDataset
 import matplotlib.image as img
 import matplotlib.pyplot as plt
 
 
-folder = '/stash/tlab/datasets/Tower/train/1'
+folder = '/stash/tlab/datasets/Tower/train/dev_test'
 
 ## Initialize class and read annotation file
 t = st()
 df = t.read_file(folder)
 
+## Make sure I understand how to use ImageDataset
+expected = os.listdir(folder + '/ims')
+expected.sort()
+vid = ImageDataset(folder + '/ims', expected)
+imgs = vid.resize(177, 100).split(0, noLabels=True)[0][0]
+img.imsave(folder + '/test000000.png', imgs[0].reshape(100, 177, 3))
+img.imsave(folder + '/test000001.png', imgs[1].reshape(100, 177, 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 ## Test on first frame in the first video clip
 print("Testing intersection methods")
 vertices = df.iloc[0, 1:9].values.reshape(4,2)
@@ -33,10 +61,16 @@ print("pix_out returns {}".format(t.class_check(490, 52, row)))
 ## Check masking
 print("Testing assign classes")
 boxes = df[df.Frame == 0].iloc[:, 1:10]
-mask = t.assign_classes(1920, 1088, boxes)
-#mask = t.assign_classes(30, 30, boxes)
+
+wn, hn, bn = t.resize_boxes(1920, 1088, 60, boxes.iloc[:, :-1])
+boxes2 = pd.DataFrame(boxes).copy()
+boxes2.iloc[:, :-1] = bn
+mask = t.assign_classes(wn, hn, boxes2)
 img.imsave(folder + '/mask0.png', mask, cmap = plt.get_cmap('gray_r'), vmin = 0,
                                 vmax = 255)
+'''
+'''
+mask = t.assign_classes(1920, 1088, boxes)
+#mask = t.assign_classes(30, 30, boxes)
 np.savetxt(folder + '/mask_data.csv', mask, delimiter = ',')
-
-
+'''
