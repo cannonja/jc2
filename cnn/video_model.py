fb34b6824c089b46c48286e22e9679c316a0200e
cannonja <jack_cannon@live.com>
2016-09-02 03:05:14 -0700

    Added logic to run new scoring with the Dice Coeff

--- cnn/video_model.py
+++ cnn/video_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from mr.datasets.tower import StanfordTower as st
+from mr.datasets.tower import TowerScaffold
 from mr.datasets.common import ImageSet
 from mr.datasets.imageDataset import ImageDataset
 from mr.learn.scaffold import Scaffold
@@ -18,7 +19,8 @@ import pickle
 
 folder = '/stash/tlab/datasets/Tower'
 file_pre = 'Neovision2-Training-Tower-'
-w_new = 36
+w_new = 10
+tau = 1
 train_folders = ['1']
 test_folders = ['13']
 '''
@@ -28,15 +30,11 @@ pickle_file.close()
 train, test, vp = pickle_data
 '''
 
-
 ## Testing preprocess - fewer images.  Need to find memory error
 videos_to_train = [os.path.join(folder, 'dev_test', 'train', 'ims')]
 videos_to_test = [os.path.join(folder, 'dev_test', 'test', 'ims')]
 train_csv = [os.path.join(folder, 'dev_test', 'train', 'train.csv')]
 test_csv = [os.path.join(folder, 'dev_test', 'test', 'test.csv')]
-
-
-
 
 
 
@@ -53,23 +51,23 @@ test_csv = [os.path.join(folder, 'test', i,
                 file_pre + str(i).zfill(3) + '.csv') for i in test_folders]
 '''
 
+
 ## Initialize class and read annotation file
 start = datetime.datetime.now()
 print ("Loading video data")
-t = st(w_new, videos_to_train, videos_to_test, train_csv, test_csv )
+t = st(w_new, videos_to_train, videos_to_test, train_csv, test_csv, tau)
 train, test, vp = t.split()
 stop = datetime.datetime.now()
 print ("Total min to load: {}".format((stop-start).total_seconds() / 60))
 
 
 
-
 ## Set up model
 print ("Building model")
 model = Scaffold()
-c = ConvolveLayer(layer = Lca(15), visualParams = vp, convSize = 4,
+c = ConvolveLayer(layer = Lca(15), visualParams = vp, convSize = 2,
             convStride = 2)
-c._init(len(train[0][0]), None)
+c.init(len(train[0][0]), None)
 model.layer(c)
 p = PoolLayer(visualParams = c.visualParams)
 model.layer(p)
@@ -84,12 +82,17 @@ print (model.layers[0].nOutputsConvolved)
 stop = datetime.datetime.now()
 train_min = (stop - start).total_seconds() / 60
 print ("Total min to train: {}".format(train_min))
+
 print ("Testing model")
 start = datetime.datetime.now()
-print (model.score(*test))
+model2 = TowerScaffold()
+xP = model.predict(test[0], False)
+y = np.asarray(test[1])
+dice = model2._calc_Dice(xP, y)
+print (dice)
+print ("Average Dice Coefficient = {}".format(np.mean(dice)))
 stop = datetime.datetime.now()
 test_min = (stop - start).total_seconds() / 60
-print ("Total min to train: {}".format(test_min))
-
+print ("Total min to test: {}".format(test_min))
 
 
